e5e21bb069c623a15578ebb550a5c7dfe7571487
Carlos Jeurissen <1038267+CarlosJeurissen@users.noreply.github.com>
2023-02-14 15:48:49 +0100

    Update to v1.40.0, move stylistic rules, add declaration-property-value-no-unknown

--- index.js
+++ index.js
@@ -24,6 +24,72 @@ const plugins = [
 ];
 
 const rules = {
+  /* stylelint-config-standard stylistic rules */
+  'at-rule-name-case': 'lower',
+  'at-rule-name-space-after': 'always-single-line',
+  'at-rule-semicolon-newline-after': 'always',
+  'block-closing-brace-empty-line-before': 'never',
+  'block-closing-brace-newline-after': 'always',
+  'block-closing-brace-newline-before': 'always-multi-line',
+  'block-closing-brace-space-before': 'always-single-line',
+  'block-opening-brace-newline-after': 'always-multi-line',
+  'block-opening-brace-space-after': 'always-single-line',
+  'block-opening-brace-space-before': 'always',
+  'color-hex-case': 'lower',
+  'declaration-bang-space-after': 'never',
+  'declaration-bang-space-before': 'always',
+  'declaration-block-semicolon-newline-after': 'always-multi-line',
+  'declaration-block-semicolon-space-after': 'always-single-line',
+  'declaration-block-semicolon-space-before': 'never',
+  'declaration-block-single-line-max-declarations': 1,
+  'declaration-block-trailing-semicolon': 'always',
+  'declaration-colon-newline-after': 'always-multi-line',
+  'declaration-colon-space-after': 'always-single-line',
+  'declaration-colon-space-before': 'never',
+  'function-comma-newline-after': 'always-multi-line',
+  'function-comma-space-after': 'always-single-line',
+  'function-comma-space-before': 'never',
+  'function-max-empty-lines': 0,
+  'function-parentheses-newline-inside': 'always-multi-line',
+  'function-parentheses-space-inside': 'never-single-line',
+  'function-whitespace-after': 'always',
+  indentation: 2,
+  'max-empty-lines': 1,
+  // 'max-line-length': 120,
+  'media-feature-colon-space-after': 'always',
+  'media-feature-colon-space-before': 'never',
+  'media-feature-name-case': 'lower',
+  'media-feature-parentheses-space-inside': 'never',
+  'media-feature-range-operator-space-after': 'always',
+  'media-feature-range-operator-space-before': 'always',
+  'media-query-list-comma-newline-after': 'always-multi-line',
+  'media-query-list-comma-space-after': 'always-single-line',
+  'media-query-list-comma-space-before': 'never',
+  'no-empty-first-line': true,
+  'no-eol-whitespace': true,
+  'no-missing-end-of-source-newline': true,
+  'number-leading-zero': 'always',
+  'number-no-trailing-zeros': true,
+  'property-case': 'lower',
+  'selector-attribute-brackets-space-inside': 'never',
+  'selector-attribute-operator-space-after': 'never',
+  'selector-attribute-operator-space-before': 'never',
+  'selector-combinator-space-after': 'always',
+  'selector-combinator-space-before': 'always',
+  'selector-descendant-combinator-no-non-space': true,
+  'selector-list-comma-newline-after': 'always',
+  'selector-list-comma-space-before': 'never',
+  'selector-max-empty-lines': 0,
+  'selector-pseudo-class-case': 'lower',
+  'selector-pseudo-class-parentheses-space-inside': 'never',
+  'selector-pseudo-element-case': 'lower',
+  'string-quotes': 'double',
+  'unit-case': 'lower',
+  'value-list-comma-newline-after': 'always-multi-line',
+  'value-list-comma-space-after': 'always-single-line',
+  'value-list-comma-space-before': 'never',
+  'value-list-max-empty-lines': 0,
+
   /* stylelint-config-standard overwrites */
   'alpha-value-notation': 'percentage',
   'color-hex-length': 'long',
@@ -287,6 +353,8 @@ const rules = {
 
   'csstools/value-no-unknown-custom-properties': true,
 
+  'declaration-property-value-no-unknown': true,
+
   'csstree/validator': {
     ignoreValue: 'env\\(',
     syntaxExtensions: false,
@@ -334,6 +402,7 @@ const rules = {
   media-feature-name-allowed-list
   media-feature-name-disallowed-list
   media-feature-name-value-allowed-list
+  media-feature-name-unit-allowed-list // todo
   property-allowed-list
   selector-attribute-name-disallowed-list
   selector-attribute-operator-allowed-list
