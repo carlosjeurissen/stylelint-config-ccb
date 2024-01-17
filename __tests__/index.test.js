0ea1a9b557af85ae89bb33134193648088ae4c92
Carlos Jeurissen <1038267+carlosjeurissen@users.noreply.github.com>
2024-01-17 16:40:18 +0100

    Improve and fix plugin testing

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,12 +1,14 @@
-/* eslint-env node, es2022, jest */
+/* eslint-env node, es2022 */
 /* eslint-disable
   global-require,
   no-autofix/strict,
-  unicorn/prefer-module,
 */
 
 import fs from 'node:fs';
 import stylelint from 'stylelint';
+import {
+  beforeEach, describe, expect, test,
+} from 'vitest';
 import config from '../dist/main.cjs';
 
 const validCss = fs.readFileSync('./__tests__/valid.css', 'utf8');
@@ -15,53 +17,53 @@ const invalidCss = fs.readFileSync('./__tests__/invalid.css', 'utf8');
 describe('flags no warnings with valid css', () => {
   let result;
 
-  beforeEach(() => {
-    result = stylelint.lint({
+  beforeEach(async () => {
+    result = await stylelint.lint({
       code: validCss,
       config: config,
     });
   });
 
-  it('did not error', () => result.then((data) => expect(data.errored).toBeFalsy()));
+  test('did not error', () => expect(result.errored).toBeFalsy());
 
-  it('flags no warnings', () => result.then((data) => expect(data.results[0].warnings).toHaveLength(0)));
+  test('flags no warnings', () => expect(result.results[0].warnings).toHaveLength(0));
 });
 
 describe('flags warnings with invalid css', () => {
   let result;
 
-  beforeEach(() => {
-    result = stylelint.lint({
+  beforeEach(async () => {
+    result = await stylelint.lint({
       code: invalidCss,
       config: config,
     });
   });
 
-  it('did error', () => result.then((data) => expect(data.errored).toBeTruthy()));
+  test('did error', () => expect(result.errored).toBeTruthy());
 
-  it('flags one warning', () => result.then((data) => expect(data.results[0].warnings).toHaveLength(3)));
+  test('flags one warning', () => expect(result.results[0].warnings).toHaveLength(3));
 
-  it('correct warning text', () => result.then((data) => expect(data.results[0].warnings[0].text).toBe(
+  test('correct warning text', () => expect(result.results[0].warnings[0].text).toBe(
     'Expected a leading zero (stylistic/number-leading-zero)',
-  )));
+  ));
 
-  it('correct warning text', () => result.then((data) => expect(data.results[0].warnings[1].text).toBe(
+  test('correct warning text', () => expect(result.results[0].warnings[1].text).toBe(
     'Expected "button" to have no more than 0 type selectors (selector-max-type)',
-  )));
+  ));
 
-  it('correct warning text', () => result.then((data) => expect(data.results[0].warnings[2].text).toBe(
+  test('correct warning text', () => expect(result.results[0].warnings[2].text).toBe(
     'Insert "0" (prettier/prettier)',
-  )));
+  ));
 
-  it('correct rule flagged', () => result.then((data) => expect(data.results[0].warnings[0].rule).toBe('stylistic/number-leading-zero')));
+  test('correct rule flagged', () => expect(result.results[0].warnings[0].rule).toBe('stylistic/number-leading-zero'));
 
-  it('correct rule flagged', () => result.then((data) => expect(data.results[0].warnings[1].rule).toBe('selector-max-type')));
+  test('correct rule flagged', () => expect(result.results[0].warnings[1].rule).toBe('selector-max-type'));
 
-  it('correct rule flagged', () => result.then((data) => expect(data.results[0].warnings[2].rule).toBe('prettier/prettier')));
+  test('correct rule flagged', () => expect(result.results[0].warnings[2].rule).toBe('prettier/prettier'));
 
-  it('correct severity flagged', () => result.then((data) => expect(data.results[0].warnings[0].severity).toBe('error')));
+  test('correct severity flagged', () => expect(result.results[0].warnings[0].severity).toBe('error'));
 
-  it('correct line number', () => result.then((data) => expect(data.results[0].warnings[0].line).toBe(2)));
+  test('correct line number', () => expect(result.results[0].warnings[0].line).toBe(2));
 
-  it('correct column number', () => result.then((data) => expect(data.results[0].warnings[0].column).toBe(8)));
+  test('correct column number', () => expect(result.results[0].warnings[0].column).toBe(8));
 });
